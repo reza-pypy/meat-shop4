cd3da0b26df46fbbca10fb702e4da04ce1a570d0
reza-pypy <pyreza54@gmail.com>
2025-02-04 13:35:00 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>گوشت شترمرغ</title>
+    
+  
+
+
+<?php include("header.html") 
+?>
+
+<div class="container" >
+    <h2>محصولات ما</h2>
+
+    <div class="gallery row">
+        <div class="col-12 col-lg">
+        <div  class="gallery-item">
+            <img  src="oil.jpg" alt="محصول 1">
+            
+           <a href="oil.php"> <h3>روغن شترمرغ</h3></a>
+            <p>روغن شترمرغ مناسب برای مالش درد و زیبایی پوست</p>
+        </div>
+        </div>
+        <div class="col-12 col-lg">
+        <div class="gallery-item">
+            <img src="meat.jpg" alt="محصول 2">
+           <a href="meat.php"> <h3>گوشت شترمرغ </h3></a>
+            <p>گوشت شترمرغ باطبع گرم وجز بهترین.</p>
+        </div>
+        </div>
+        <div class="col-12 col-lg">
+        <div class="gallery-item">
+            <img src="lh.jpg" alt="">
+            <a href="lh.php"><h3>دل جگر</h3> </a>
+            <p>دل وجگر شترمرغ مناسب برای کم خونی و بیماری.</p>
+        </div>
+        </div>
+    </div>
+</div>
+
+
+<footer>
+    <p>تمامی حقوق محفوظ است &copy; 2025</p>
+</footer>
+
+
+</body>
+</html>
---
+++ meat.php
@@ -0,0 +1,13 @@
+<style>
+    div.img{text-align: center;
+        width: 20pc;
+
+    }
+</style>
+<?php include("header.html")
+
+?>
+
+<div>
+<img src="meat.jpg" alt="maet">
+</div>
